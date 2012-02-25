bfb160b4aa0922288e68cf89911033080856be4d
Shareef Ali <shareefalis@gmail.com>
2012-02-25 15:44:11 -0500

    epicmtd: Let's sources makefile handle the opitmization for Samsung Exynos 3110. renamed cflag define to arch_variant_cflags override old param and adds -mtune flags and all of neon features soc.
    Change-Id: I335fdcfb6a5b094c8b75cbb9ec9ece72c1621ca9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,10 +12,12 @@ BOARD_USES_LIBSECRIL_STUB := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 ARCH_ARM_HAVE_TLS_REGISTER := true
-ANDROID_ARM_LINKER := true
+arch_variant_cflags := \
+    -march=armv7-a \
+    -mtune=cortex-a8 \
+    -mfloat-abi=softfp \
+    -mfpu=neon
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
