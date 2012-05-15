707de20f5df4fe711cf5a5eb7e5bc439a63b480a
Scott <Tortel1210@gmail.com>
2012-05-15 16:10:17 -0400

    Add EpicParts with option to disable capacitive backlights
    Change-Id: I3b78af09a3c29f5efecad5f5edea1aafb11f91f9

--- epicmtd.mk
+++ epicmtd.mk
@@ -95,6 +95,12 @@ PRODUCT_PACKAGES += \
 # tvout 
 PRODUCT_PACKAGES += \
 	tvouthack
+
+# Device-specific packages
+PRODUCT_PACKAGES += \
+    EpicParts
+
+
 # apns config file
 PRODUCT_COPY_FILES += \
         device/samsung/epicmtd/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
