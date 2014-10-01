310f58ab16e58963efb024ba5d3db5f1edfa3bff
Brian Beloshapka <bbelos@hotmail.com>
2014-10-01 14:32:05 -0400

    epicmtd: Switch to mini phone config.
    Excludes common_full.mk (Live wallpapers, video editing, etc)
    which frees up 20+ MB from our overloaded /system.
    
    Change-Id: I5546d0d23fcbdc3661d19df8b8393f8368fda0f7
    (cherry picked from commit cbd430d50096979ac9aabc8247e16d32ca4e6752)

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := EpicMTD
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/epicmtd/full_epicmtd.mk)
